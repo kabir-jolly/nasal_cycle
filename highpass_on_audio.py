f4dfbab5fede61c737762ae20062fc68600f6a70
Kabir Jolly <kabir.s.jolly@gmail.com>
2021-08-10 13:46:58 -0700

    added support for highpass filtering. Now audio is processed properly and still appears in the final .wav file. Fixed issue with following the methodology from the microphone paper.

--- highpass_on_audio.py
+++ highpass_on_audio.py
@@ -3,28 +3,30 @@ import numpy as np
 import os
 from scipy.io import wavfile
 from scipy.signal import butter, lfilter
+from prelim_mic_testing import visualize
 
 WAV_FILE_NAME = input("Enter a .wav filepath to apply a bandpass filter on it: ")
-lowcut = 10.0
-highcut = 150.0
+highcut = 10000.0 # 10kHz highpass filter
 FRAME_RATE = 44100
 # def orders 5-5-6
-def butter_bandpass(lowcut, highcut, fs, order=3):
+def butter_bandpass(highcut, fs, order=3):
     nyq = 0.5 * fs
-    low = lowcut / nyq
     high = highcut / nyq
-    b, a = butter(order, [low, high], btype='band')
+    b, a = butter(order, high, btype='highpass')
     return b, a
 
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=3):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+def butter_bandpass_filter(data, highcut, fs, order=3):
+    b, a = butter_bandpass(highcut, fs, order=order)
     y = lfilter(b, a, data)
     return y
 
 def bandpass_filter(buffer):
-    return butter_bandpass_filter(buffer, lowcut, highcut, FRAME_RATE, order=3)
+    return butter_bandpass_filter(buffer, highcut, FRAME_RATE, order=3)
 
 samplerate, data = wavfile.read(WAV_FILE_NAME)
 assert samplerate == FRAME_RATE
 filtered = np.apply_along_axis(bandpass_filter, 0, data).astype('int16')
-wavfile.write('filtered_audio.wav', samplerate, filtered)
+filename = 'filtered_audio_highpass.wav'
+wavfile.write(filename, samplerate, filtered)
+
+visualize(filename)
